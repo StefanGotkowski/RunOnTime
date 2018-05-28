18ab10e189876382990c5914897d822743ac49e8
Stefan Gotkowski <stefan.gotkowski@gmail.com>
2018-05-28 14:02:27 +0200

    Add files via upload

---
+++ src/RunAtTime.h
@@ -0,0 +1,22 @@
+/*
+  RunAtTime.h (or BlinkWithoutDelay)- Library for launching an task after x milisecond
+  Created by Stefan Gotkowski, 27 mai 2018
+  Released into the public domain.
+*/
+#ifndef RunAtTime_h
+
+  #define RunAtTime_h
+  #include "Arduino.h"
+
+  class RunAtTime
+  {
+    public:
+      RunAtTime( unsigned long milisec );
+      boolean isTime();
+      
+    private:
+      unsigned long _previousMillis = 0;
+      unsigned long _milisec;
+  };
+
+#endif
